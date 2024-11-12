337484a2714c5edfd47d3d9e15315716245bac88
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-11-11 21:26:50 -0500

    initial commit

---
+++ src/main/java/org/launchcode/techjobsauth/models/dto/RegisterFormDTO.java
@@ -0,0 +1,31 @@
+package org.launchcode.techjobsauth.models.dto;
+
+public class RegisterFormDTO {
+    private String verifyPassword;
+    private String username;
+    private String password;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
